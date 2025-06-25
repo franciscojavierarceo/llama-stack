e20310689d4b11638827ea7689882cf5081b13cf
Francisco Javier Arceo <farceo@redhat.com>
2025-06-25 16:10:18 -0400

    removing sqlite-vec import
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- llama_stack/providers/inline/vector_io/sqlite_vec/sqlite_vec.py
+++ llama_stack/providers/inline/vector_io/sqlite_vec/sqlite_vec.py
@@ -31,7 +31,6 @@ from llama_stack.providers.utils.memory.vector_store import (
     EmbeddingIndex,
     VectorDBWithIndex,
 )
-from llama_stack.providers.utils.vector_io.chunk_utils import generate_chunk_id
 
 logger = logging.getLogger(__name__)
 
